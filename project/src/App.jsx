6b4021b4e186e9b878f42b906e0478c1827cb23f
asijasijaricc <asijasijaricc12@gmail.com>
2023-12-22 20:58:42 +0100

    idk

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,16 +1,17 @@
-import img from "../public/as..png";
+import "./app.css";
+import img from "../public/as.jpg";
 
 function App() {
   return (
-    <div>
-      <div>
+    <div className="nav">
+      <div className="logic">
         <img src={img} alt="" />
       </div>
-      <div>
-        <p>home</p>
-        <p>categories</p>
-        <p>articles</p>
-        <p></p>
+      <div className="linkovi">
+        <a href="">home</a>
+        <a href="">categories</a>
+        <a href="">articles</a>
+        <a href="">about us</a>
       </div>
     </div>
   );
