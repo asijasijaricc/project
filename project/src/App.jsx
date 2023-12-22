a34cfdebd7dcf953ca8f68ce90d434bd57885255
asijasijaricc <asijasijaricc12@gmail.com>
2023-12-22 20:47:43 +0100

    still dk

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,8 +1,19 @@
-import { useEffect, useState } from "react";
-import "./App.css";
+import img from "../public/as..png";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <img src={img} alt="" />
+      </div>
+      <div>
+        <p>home</p>
+        <p>categories</p>
+        <p>articles</p>
+        <p></p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
