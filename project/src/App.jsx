270fbe810bceca1a4d359ca26e02d6fdc5e4b54a
asijasijaricc <asijasijaricc12@gmail.com>
2023-12-25 19:15:17 +0100

    idk

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -3,15 +3,35 @@ import img from "../public/as.jpg";
 
 function App() {
   return (
-    <div className="nav">
-      <div className="logic">
-        <img src={img} alt="" />
+    <div>
+      <div className="header">
+        <div className="logic">
+          <img src={img} alt="" />
+        </div>
+        <div className="linkovi">
+          <ul>home</ul>
+          <ul>articles</ul>
+          <ul>contact</ul>
+          <ul>about</ul>
+        </div>
+        <input
+          className="pretrazi"
+          type="text"
+          disabled={false}
+          placeholder="search"
+          size="lg"
+          variant="soft"
+        />
       </div>
-      <div className="linkovi">
-        <a href="">home</a>
-        <a href="">categories</a>
-        <a href="">articles</a>
-        <a href="">about us</a>
+      <div className="pocetak">
+        <h4>Hi ! I am as.</h4>
+        <hr />
+        <p>
+          Welcome aboard! At as. we're passionate about business. Dive into a
+          treasure trove of articles, insights, and stories crafted just for
+          you.Ready to embark on an exciting journey? Start exploring below and
+          uncover the endless possibilities of world of business with us!
+        </p>
       </div>
     </div>
   );
