6b483cc4b56306e9976ce51e6064ab5c3645ae0e
asijasijaricc <asijasijaricc12@gmail.com>
2024-01-03 19:34:03 +0100

    i dont know

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,6 +1,11 @@
 import "./app.css";
 import img from "../public/as.jpg";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGlasses } from "@fortawesome/free-solid-svg-icons";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { faTrophy } from "@fortawesome/free-solid-svg-icons";
+import Divider from "../component/dev/divider";
 
 const router = createBrowserRouter([
   { path: "/", element: <p></p> },
@@ -57,16 +62,33 @@ function App() {
           </div>
         </div>
       </div>
+      <Divider />
+      <div className="cilj">
+        <h1>Our achievements !</h1>
+        <p>
+          Celebrating our journey, our blog has become a thriving community of
+          50,000 daily readers, a testament to the engaging content we share.
+          Each post is embraced with 15,000 likes, a heartfelt acknowledgment
+          from our incredible audience. Over the past two years, our dedication
+          and creativity have earned us 146 awards, milestones that reflect our
+          commitment to delivering meaningful, entertaining, and award-winning
+          content. With every achievement, we're inspired to keep pushing
+          boundaries and crafting content that resonates with our audience.
+        </p>
+      </div>
       <div className="stafic">
         <div className="citaci">
+          <FontAwesomeIcon icon={faGlasses} className="naocare" />
           <h1>50k</h1>
           <p>readers per day</p>
         </div>
         <div className="lajkovi">
+          <FontAwesomeIcon icon={faHeart} className="srce" />
           <h1>15K</h1>
-          <p>likes on the post</p>
+          <p>hearts on the post</p>
         </div>
         <div className="nagrade">
+          <FontAwesomeIcon icon={faTrophy} className="trofej" />
           <h1>146</h1>
           <p>social media awards</p>
         </div>
